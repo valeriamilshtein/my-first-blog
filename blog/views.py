898e08dcc96cb42242866e29eadc00b05e34eda5
valeriamilshtein <[valeriamilshtein@gmail.com]>
2021-11-25 22:27:44 -0500

    added views to create/edit blog post inside the site

--- blog/views.py
+++ blog/views.py
@@ -31,7 +31,7 @@ def post_edit(request, pk):
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
-            post = form.save(commit=True)
+            post = form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
