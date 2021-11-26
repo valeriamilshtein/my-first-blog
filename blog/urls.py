e5f211e2475a8501ea0f831a91ea542f1a722ae3
valeriamilshtein <[valeriamilshtein@gmail.com]>
2021-11-25 23:01:49 -0500

    added publish button

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
-    url(r'^post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
 ]
