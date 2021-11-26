6c55dfa22d97c9df5d13194cdf72205a805fa635
valeriamilshtein <[valeriamilshtein@gmail.com]>
2021-11-25 23:00:05 -0500

    added publish button

--- blog/views.py
+++ blog/views.py
@@ -39,3 +39,8 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
