0860cbb2bda7e86359f3b1b308733d9e9fabb72f
valeriamilshtein <[valeriamilshtein@gmail.com]>
2021-11-25 23:04:27 -0500

    removed publish button

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
-    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -39,8 +39,3 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
-
-def post_publish(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    post.publish()
-    return redirect('post_detail', pk=pk)
