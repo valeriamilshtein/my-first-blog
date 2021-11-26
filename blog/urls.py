f72df902bd9e406ec7aa2d26a3ab0d05aeee4405
valeriamilshtein <[valeriamilshtein@gmail.com]>
2021-11-25 22:47:02 -0500

    added post_list.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,4 @@
+from sys import path
 from django.conf.urls import url
 from . import views
 
@@ -6,4 +7,5 @@ urlpatterns = [
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
 ]
